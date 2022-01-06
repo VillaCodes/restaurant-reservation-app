2f74f2bceb68f62391b7910399b85943ae39dbe6
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-01-06 12:43:58 -0800

    Developing reservations controller
    Added read function and worked on middleware.

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,5 +1,56 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require(".reservations.service")
+
+
+
+//all the fields used for reservations
+
+const VALID_RESERVATION_FIELDS = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people",
+];
+
+//helper function to be used in validation
+function _validateTime(str) {
+  const [hour, minute] = str.split(":");
+
+  if (hour.length > 2 || minute.length > 2) {
+    return false;
+  }
+  if (hour < 1 || hour > 23) {
+    return false;
+  }
+  if (minute < 0 || minute > 59) {
+    return false;
+  }
+  return true;
+}
+
+//validation below
+
+function hasData(req, res, next) {
+  if (req.body.data) {
+    return next()
+  }
+  next({status: 400, message: "Body must have data property"})
+}
+
+function isNotOnTuesday(req, res, next) {
+  const { reservation_date } = req.body.data;
+  const [year, month, day] = reservation_date.split("-");
+  const date = new Date(`${month} ${day}, ${year}`);
+  res.locals.date = date;
+  if (date.getDay() === 2) {
+    return next({ status: 400, message: "Location is closed on Tuesdays" });
+  }
+  next();
+}
+
+//crud below
 /**
  * List handler for reservation resources
  */
@@ -7,6 +58,12 @@ async function list(req, res) {
   const data = await service.list();
 
   res.json({data});
+}
+
+async function read (req, res) {
+  const reservation = res.locals.reservation
+
+  res.json({reservation});
 }
 
 module.exports = {
