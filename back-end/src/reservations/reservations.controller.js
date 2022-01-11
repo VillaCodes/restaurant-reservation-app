2d89dfcc3e6e647cea90f5c5f2e48a88c427b444
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-01-11 13:37:44 -0800

    Update reservations.controller.js

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,7 +5,20 @@ const service = require("./reservations.service")
 
 //all the fields used for reservations
 
-const VALID_RESERVATION_FIELDS = [
+const VALID_PROPERTIES = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people",
+  "status",
+  "reservation_id",
+  "created_at",
+  "updated_at",
+];
+
+const REQUIRED_FIELDS = [
   "first_name",
   "last_name",
   "mobile_number",
@@ -30,7 +43,42 @@ function _validateTime(str) {
   return true;
 }
 
-//validation below
+//validation below//
+
+
+//checks the valid properties array above to make sure there are no unwanted properties in the request
+
+function hasOnlyValidProperties(req, res, next) {
+  const {data = {}} = req.body
+
+  const invalidProperties = Object.keys(data).filter((property) => {
+    return !VALID_PROPERTIES.includes(property)
+  })
+
+  if (invalidProperties) {
+    return next({status: 400, message: `Invalid field(s): ${invalidStatuses.join(", ")}`})
+  }
+
+  next();
+}
+
+//check @param reservation_id to make sure the matching res exists
+
+function reservationExists(req, res, next) {
+  const { reservation_id } = req.params;
+  const reservation = await service.read(reservation_id)
+
+  if (reservation) {
+    res.locals.reservation = reservation;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Reservation ${req.params.reservation_id} does not exist.`,
+  });
+}
+
+//validate that request has data/is not null
 
 function hasData(req, res, next) {
   if (req.body.data) {
@@ -38,6 +86,8 @@ function hasData(req, res, next) {
   }
   next({status: 400, message: "Body must have data property"})
 }
+
+//make sure reservation is not on a Tuesday
 
 function isNotOnTuesday(req, res, next) {
   const { reservation_date } = req.body.data;
@@ -68,5 +118,5 @@ async function read (req, res) {
 
 module.exports = {
   list: asyncErrorBoundary(list),
-  read: [asyncErrorBoundary]
+  read: [asyncErrorBoundary(reservationExists), read]
 };
