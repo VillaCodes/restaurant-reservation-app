221a4a0cd2d346649e49eabda9c87cb4d179c615
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-01-24 20:31:11 -0800

    Update api.js

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-process.env.NODE_ENV === 'production' ? 'https://villacodes-reservations.herokuapp.com/' : 'http://localhost:5000';
+process.env.NODE_ENV === 'production' ? 'https://villacodes-reservations.herokuapp.com' : 'http://localhost:5000';
 //  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 /**
